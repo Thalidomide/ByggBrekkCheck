e10d98758132eb67fa4975bae4ac2b4080d16cfb
Olav Jensen <olav.jensen@gmail.com>
2009-10-24 15:54:23 +0200

    Refaktorering

--- src/main/java/no/teamjava/byggbrekker/gui/CredentialsPanel.java
+++ src/main/java/no/teamjava/byggbrekker/gui/CredentialsPanel.java
@@ -7,6 +7,7 @@ import java.awt.HeadlessException;
 import java.awt.Rectangle;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import javax.swing.JPanel;
@@ -119,13 +120,7 @@ public class CredentialsPanel extends JPanel {
 	}
 
 	private KeyListener getLoginKeyListener() {
-		return new KeyListener() {
-			@Override
-			public void keyTyped(KeyEvent e) {}
-
-			@Override
-			public void keyPressed(KeyEvent e) {}
-
+		return new KeyAdapter() {
 			@Override
 			public void keyReleased(KeyEvent e) {
 				if (e.getKeyCode() == KeyEvent.VK_ENTER) {
