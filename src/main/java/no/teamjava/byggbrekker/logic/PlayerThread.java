739b61f38abc8664272e3e3b55b282448f59df7c
Thalidomide <olav.jensen@gmail.com>
2009-12-08 12:58:56 +0100

    Repeat av musikkavspilling ved brekk

--- src/main/java/no/teamjava/byggbrekker/logic/PlayerThread.java
+++ src/main/java/no/teamjava/byggbrekker/logic/PlayerThread.java
@@ -2,7 +2,9 @@ package no.teamjava.byggbrekker.logic;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 
+import javazoom.jl.decoder.JavaLayerException;
 import javazoom.jl.player.Player;
 import no.teamjava.byggbrekker.entities.Settings;
 
@@ -14,6 +16,7 @@ public class PlayerThread extends Thread {
 
 	private Player player;
 	private Jukebox jukebox;
+	private boolean play;
 
 	public PlayerThread() {
 		jukebox = new Jukebox(Settings.BROKEN_BUILD_MP3_PATH);
@@ -21,25 +24,38 @@ public class PlayerThread extends Thread {
 
 	@Override
 	public void run() {
+		play = true;
+
 		try {
-			System.out.println("Starte med å spille musikk..");
-			File toBePlayed = jukebox.getMp3ToBePlayed();
-			System.out.println("Skal spille: " + toBePlayed.getName() + ". Lage file input stream");
-			FileInputStream inputStream = new FileInputStream(toBePlayed);
-			System.out.println("Laget FileInputStream, lage player..");
-			player = new Player(inputStream);
-			System.out.println("Laget player, skal spille");
-			player.play();
-			System.out.println("Nå skal sangen være i gang");
+			while (play) {
+				if (player == null || player.isComplete()) {
+					System.out.println("Spille ny sang..");
+					File toBePlayed = jukebox.getMp3ToBePlayed();
+					playMp3(toBePlayed);
+				}
+				sleep(1000);
+			}
 		} catch (Exception e1) {
 			e1.printStackTrace();
+			play = false;
 		}
+	}
+
+	private void playMp3(File toBePlayed) throws FileNotFoundException, JavaLayerException {
+		System.out.println("Skal spille: " + toBePlayed.getName() + ". Lage file input stream");
+		FileInputStream inputStream = new FileInputStream(toBePlayed);
+		System.out.println("Laget FileInputStream, lage player..");
+		player = new Player(inputStream);
+		System.out.println("Laget player, skal spille");
+		player.play();
+		System.out.println("Nå skal sangen være i gang");
 	}
 
 	public void stopPlayer() {
 		if (player == null) {
 			return;
 		}
+		play = false;
 		player.close();
 	}
 }
