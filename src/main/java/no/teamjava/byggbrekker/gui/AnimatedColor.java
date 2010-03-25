2d57302e2c731172587eacb30fd47ad762306e66
unknown <olav.jensen@gmail.com>
2010-03-25 08:31:31 +0100

    Oppdatert animasjon av farge

--- src/main/java/no/teamjava/byggbrekker/gui/AnimatedColor.java
+++ src/main/java/no/teamjava/byggbrekker/gui/AnimatedColor.java
@@ -8,37 +8,92 @@ import java.awt.Color;
  */
 public class AnimatedColor {
 
-	private final int initialRed, initialGreen, initialBlue;
-	private int red, green, blue;
-
-	private boolean up = true;
-	private int step = 5;
+	private final ColorValue red, green, blue;
+	private ColorValue animating;
 
 	public AnimatedColor(int initialRed, int initialGreen, int initialBlue) {
-		this.initialRed = initialRed;
-		this.initialGreen = initialGreen;
-		this.initialBlue = initialBlue;
+		red = new ColorValue(initialRed);
+		green = new ColorValue(initialGreen);
+		blue = new ColorValue(initialBlue);
 
-		red = initialRed;
-		green = initialGreen;
-		blue = initialBlue;
+		red.setNext(green);
+		green.setNext(red);
+
+		animating = red;
 	}
 
 	public Color getNext() {
+		animate();
+
+		return new Color(red.getValue(), green.getValue(), blue.getValue());
+	}
+
+	private void animate() {
+		boolean hitMaxLow = animating.animate();
+		if (hitMaxLow) {
+			animating = animating.getNext();
+		}
+	}
+}
+class ColorValue {
+	private final int baseValue, minValue, maxValue;
+	private final static int hiLowRange = 100;
+	private final static int step = 2;
+
+	private int value;
+	private boolean up = true;
+	private ColorValue next;
+
+	ColorValue(int baseValue) {
+		this.baseValue = baseValue;
+		minValue = getMinValue();
+		maxValue = getMaxValue();
+		value = baseValue;
+	}
+
+	/**
+	 * @return true if hitting max/low value
+	 */
+	public boolean animate() {
+		boolean hitMaxLow = false;
+
 		if (up) {
-			red += step;
-			if (red >= 255) {
-				red = 255;
+			value += step;
+			if (value >= maxValue) {
+				value = maxValue;
 				up = false;
+				hitMaxLow = true;
 			}
 		} else {
-			red -= step;
-			if (red <= 0) {
-				red = 0;
+			value -= step;
+			if (value <= minValue) {
+				value = minValue;
 				up = true;
+				hitMaxLow = true;
 			}
 		}
+		return hitMaxLow;
+	}
 
-		return new Color(red, green, blue);
+	public int getValue() {
+		return value;
+	}
+
+	public ColorValue getNext() {
+		return next;
+	}
+
+	public void setNext(ColorValue next) {
+		this.next = next;
+	}
+
+	private int getMaxValue() {
+		int res = baseValue + hiLowRange;
+		return res <= 255 ? res : 255;
+	}
+
+	private int getMinValue() {
+		int res = baseValue - hiLowRange;
+		return res >= 0 ? res : 255;
 	}
 }
